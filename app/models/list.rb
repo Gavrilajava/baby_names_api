94f6368f5ec439f525602bb13777243877e0b344
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-07-08 13:55:39 -0500

    validation and models behavior

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,22 @@
 class List < ApplicationRecord
 
   has_many :names
+
+  validate :validate_records_number, on: :create
+  validates :alphanumeric, uniqueness: true
+
+  def validate_records_number
+    self.errors.add(:base, "Maximum number of lists exceeded") if List.count > 12**62
+  end
+
+  def initialize(params=nil)
+    # If it were a real application, I'd suggest using uuid as an id.
+    alphanumeric = ""
+    loop do
+      alphanumeric = SecureRandom.alphanumeric(12)
+      break if !List.find_by(alphanumeric: alphanumeric)
+    end
+    params_with_an = {alphanumeric: alphanumeric}.merge(params.to_h)
+    super(params_with_an)
+  end
 end
--- app/models/name.rb
+++ app/models/name.rb
@@ -1,4 +1,23 @@
 class Name < ApplicationRecord
 
   belongs_to :list
+
+  before_validation(on: :create) do
+    self.name = self.name.strip
+  end
+
+  validate :validate_name, on: :create
+
+  
+
+  def validate_name
+    if self.list.names.find_by("LOWER(name) = ?", self.name.downcase)
+      self.errors.add(:base, "Name is not unique")
+    end
+    # if !(/^[a-zA-Z]+(\s{0,1}[a-zA-Z])*$/ === self.name) don't know why it isn't working
+    if !(/^[a-zA-Z\s]*$/ === self.name) || self.name.split(" ").length > 2
+      self.errors.add(:base, "Name is not valid")
+    end
+  end
+
 end
